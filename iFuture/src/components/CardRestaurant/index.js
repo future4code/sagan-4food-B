257e4645369581a4296a3db19768d862b5edbb92
Rosana Rezende <rezende_rosana@hotmail.com>
2020-04-14 10:54:16 -0300

    CardRestaurant

--- iFuture/src/components/CardRestaurant/index.js
+++ iFuture/src/components/CardRestaurant/index.js
@@ -1,15 +1,51 @@
 import React from 'react'
+
 import * as S from './styles'
+import { CardContent } from '@material-ui/core'
 
 function CardRestaurant(props){
 
-    const { restaurant} = props
+    const { restaurant } = props
+
+    const redirectRestaurant = (restaurantId) => {
+        // chamar a action que redireciona pro restaurant detail
+
+    }
 
     return (
         <S.CardRestaurantWrapper>
-            {restaurant.name}
+            <S.CardImage onClick={() => redirectRestaurant(restaurant.id)}>
+                <S.Image 
+                    image={restaurant.logoUrl}
+                    title={restaurant.name}
+                />
+            </S.CardImage>
+            <CardContent>
+                <S.RestaurantName>
+                    {restaurant.name}
+                </S.RestaurantName>
+                <S.InfoFooter>
+                    <S.RestaurantInfo>
+                        {restaurant.deliveryTime} min
+                    </S.RestaurantInfo>
+                    <S.RestaurantInfo>
+                        Frete {restaurant.shipping.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}
+                    </S.RestaurantInfo>
+                </S.InfoFooter>
+            </CardContent>
         </S.CardRestaurantWrapper>
     )
 }
 
 export default CardRestaurant
+
+// {
+//     id: "7",
+//     deliveryTime: 20,
+//     category: "Italiana",
+//     description: "Restaurante Self Service e lanchonete localizado no Laboratório Nacional de Computação Científica",
+//     shipping: 2,
+//     address: "Rua Barão do Rio Branco, 98 - Centro",
+//     name: "Cantina Mamma Perrotta 2",
+//     logoUrl: "http://soter.ninja/futureFoods/logos/cantinamammaperrotta.jpg"
+//   },
--- iFuture/src/components/CardRestaurant/styles.js
+++ iFuture/src/components/CardRestaurant/styles.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
-import { Paper } from '@material-ui/core'
+import { Card, CardActionArea, CardMedia, Typography } from '@material-ui/core'
 
-export const CardRestaurantWrapper = styled(Paper)`
+export const CardRestaurantWrapper = styled(Card)`
     margin-bottom: 8px;
     height: 188px;
     border-radius: 8px;
@@ -9,3 +9,38 @@ export const CardRestaurantWrapper = styled(Paper)`
     box-shadow: none;
 
 `
+
+export const CardImage = styled(CardActionArea)`
+    height: 120px;
+`
+
+export const Image = styled(CardMedia)`
+    height: 100%;
+`
+
+export const InfoFooter = styled.div`
+    display: flex;
+    justify-content: space-between;
+    margin-top: 4px;
+`
+
+export const RestaurantName = styled(Typography)`
+    color: #e8222e;
+    font-size: 16px;
+    font-weight: normal;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: normal;
+    letter-spacing: -0.39px;
+`
+
+export const RestaurantInfo = styled(Typography)`
+    color: #b8b8b8;
+    font-size: 16px;
+    font-weight: normal;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: normal;
+    letter-spacing: -0.39px;
+`
+
