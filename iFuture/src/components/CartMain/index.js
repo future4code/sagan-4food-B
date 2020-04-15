6fa29fe69a4efb49af1902547bb8c6bd08da8cb3
Rosana Rezende <rezende_rosana@hotmail.com>
2020-04-15 20:43:07 -0300

    refatorado

---
+++ iFuture/src/components/CartMain/index.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import * as S from './styles'
+
+import CardFood from '../CardFood'
+
+function CartMain(props) {
+    const { restaurant } = props
+    
+    return (
+        <main>
+            { restaurant.products.length === 0
+                
+                ?
+                <S.EmptyCartWrapper>
+                    <S.Text>
+                        Carrinho Vazio
+                    </S.Text>
+                </S.EmptyCartWrapper>
+
+                :
+                <S.FullCartWrapper>
+                    
+                    <S.DivMain>
+                        <S.Text red> {restaurant.name} </S.Text>
+                        <S.Text> {restaurant.address} </S.Text>
+                        <S.Text> {restaurant.deliveryTime - 10} - {restaurant.deliveryTime} min </S.Text>
+                    </S.DivMain>
+
+                    {restaurant.products.map(item => (
+                        <CardFood item={item} />
+                    ))}
+
+                </S.FullCartWrapper>
+            }
+        </main>
+    )
+}
+
+export default CartMain
