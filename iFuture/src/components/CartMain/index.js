03e2f4108d7af0b7b9cd0ee6490f3f7defcf2295
Rosana Rezende <rezende_rosana@hotmail.com>
2020-04-17 09:58:49 -0300

    deixando pronto pra filtrar no carrinho

--- iFuture/src/components/CartMain/index.js
+++ iFuture/src/components/CartMain/index.js
@@ -1,14 +1,22 @@
 import React from 'react'
+import { connect } from 'react-redux'
+
 import * as S from './styles'
 
 import CardFoodInCart from '../CardFoodInCart'
 
 function CartMain(props) {
-    const { restaurant } = props
-    
+    const { restaurant, infoQuantity } = props
+    console.log(infoQuantity)
+
+    // PRECISO FILTRAR, PRA NÃO MANDAR TUDO
+    // dentro do restaurant.products encontre os ids q estão no infoQuantity
+    // const filteredList = restaurant.products.filter(item => 
+    //     item.id === x)
+
     return (
         <main>
-            { restaurant.products.length === 0
+            {restaurant.products.length === 0
                 ?
                 <S.EmptyCartWrapper>
                     <S.Text>
@@ -18,16 +26,19 @@ function CartMain(props) {
 
                 :
                 <S.FullCartWrapper>
-                    
+
                     <S.DivMain>
                         <S.Text red> {restaurant.name} </S.Text>
                         <S.Text> {restaurant.address} </S.Text>
                         <S.Text> {restaurant.deliveryTime - 10} - {restaurant.deliveryTime} min </S.Text>
                     </S.DivMain>
 
-                    {restaurant.products.map(item => (
-                        <CardFoodInCart item={item} />
-                    ))}
+                                        
+                    {/* chamar filteredList aqui */}
+                    {restaurant.products
+                        .map(item => (
+                            <CardFoodInCart item={item} />
+                        ))}
 
                 </S.FullCartWrapper>
             }
@@ -35,4 +46,8 @@ function CartMain(props) {
     )
 }
 
-export default CartMain
+const mapStateToProps = state => ({
+    infoQuantity: state.food.infoQuantity
+})
+
+export default connect(mapStateToProps)(CartMain)
